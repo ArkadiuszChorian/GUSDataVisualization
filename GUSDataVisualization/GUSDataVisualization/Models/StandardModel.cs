aa4d050fb91d1185dd868cec5fb2173bbe4ddba2
lotafak <patrykk.93@gmail.com>
2016-12-17 15:24:07 +0100

    Same as before

---
+++ GUSDataVisualization/GUSDataVisualization/Models/StandardModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoRepository;
+
+namespace GUSDataVisualization.Models
+{
+    public class StandardModel : IEntity<string>
+    {
+        public string Id { get; set; }
+        public string Kod { get; set; }
+        public string Etykieta { get; set; }
+        public string Rok { get; set; }
+        public string Wartosc { get; set; }
+        public string Jednostka { get; set; }
+        public string Kategoria { get; set; }
+    }
+}
