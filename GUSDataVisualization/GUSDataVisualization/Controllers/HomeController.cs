f212319557c810db22e1680917bc56091ef26930
Arkadiusz Chorian <arkadiusz.chorian@gmail.com>
2017-01-05 17:16:52 +0100

    Added request model. Simple implementation of getting data in GetData action in Home controller.

--- GUSDataVisualization/GUSDataVisualization/Controllers/HomeController.cs
+++ GUSDataVisualization/GUSDataVisualization/Controllers/HomeController.cs
@@ -2,15 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using GUSDataVisualization.Models;
 
 namespace GUSDataVisualization.Controllers
-{
-    [Authorize]
+{    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetData(RequestModel rm)
+        {
+            var projection = DAL.Instance.Dane.Where(d => true);
+
+            if (!string.IsNullOrEmpty(rm.Kategoria1))
+            {
+                projection = projection.Where(d => d.Kategoria1 == rm.Kategoria1);
+            }
+            if (!string.IsNullOrEmpty(rm.Kategoria2))
+            {
+                projection = projection.Where(d => d.Kategoria2 == rm.Kategoria2);
+            }
+            if (!string.IsNullOrEmpty(rm.Kategoria3))
+            {
+                projection = projection.Where(d => d.Kategoria3 == rm.Kategoria3);
+            }
+            if (!string.IsNullOrEmpty(rm.RokOd))
+            {
+                projection = projection.Where(d => int.Parse(d.Rok) >= int.Parse(rm.RokOd));
+            }
+            if (!string.IsNullOrEmpty(rm.RokDo))
+            {
+                projection = projection.Where(d => int.Parse(d.Rok) <= int.Parse(rm.RokDo));
+            }
+            if (!string.IsNullOrEmpty(rm.Etykieta1))
+            {
+                projection = projection.Where(d => d.Etykieta1.Contains(rm.Etykieta1));
+            }
+            if (!string.IsNullOrEmpty(rm.Etykieta2))
+            {
+                projection = projection.Where(d => d.Etykieta2.Contains(rm.Etykieta2));
+            }
+            if (!string.IsNullOrEmpty(rm.Kod))
+            {
+                projection = projection.Where(d => d.Kod == rm.Kod);
+            }
+            if (!string.IsNullOrEmpty(rm.WartoscOd))
+            {
+                projection = projection.Where(d => int.Parse(d.Wartosc) >= int.Parse(rm.WartoscOd));
+            }
+            if (!string.IsNullOrEmpty(rm.WartoscDo))
+            {
+                projection = projection.Where(d => int.Parse(d.Wartosc) <= int.Parse(rm.WartoscDo));
+            }
+
+            return Json(projection.ToList());
         }
     }
 }
---
+++ GUSDataVisualization/GUSDataVisualization/Models/RequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GUSDataVisualization.Models
+{
+    public class RequestModel
+    {
+        public string Kod { get; set; }
+        public string RokOd { get; set; }
+        public string RokDo { get; set; }      
+        public string Etykieta1 { get; set; }
+        public string Etykieta2 { get; set; }
+        public string WartoscOd { get; set; }
+        public string WartoscDo { get; set; }
+        public string Kategoria1 { get; set; }
+        public string Kategoria2 { get; set; }
+        public string Kategoria3 { get; set; }
+    }
+}
